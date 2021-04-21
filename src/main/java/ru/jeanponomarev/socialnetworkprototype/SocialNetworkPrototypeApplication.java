4c098d4d4eaaa4f3f3911c11905c0e9e0c4b6df0
Jean Ponomarev <Jean.Ponomarev@nordigy.ru>
2021-04-22 01:23:55 +0300

    Initial commit

---
+++ src/main/java/ru/jeanponomarev/socialnetworkprototype/SocialNetworkPrototypeApplication.java
@@ -0,0 +1,13 @@
+package ru.jeanponomarev.socialnetworkprototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialNetworkPrototypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialNetworkPrototypeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/jeanponomarev/socialnetworkprototype/SocialNetworkPrototypeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.jeanponomarev.socialnetworkprototype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetworkPrototypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
